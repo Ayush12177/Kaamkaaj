99fe7561242024216f3d7dd9f990e8579483c137
Ayush Gupta <ayushgupta96396@gmail.com>
2025-08-18 13:58:44 +0530

    added

--- models/seeker.js
+++ models/seeker.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/database");
+// mongoose.connect("mongodb://127.0.0.1:27017/database");
+require('dotenv').config();
+const db = process.env.MONGO_URI 
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 const seekerSchema = mongoose.Schema({
     name: String,
     email: String,
